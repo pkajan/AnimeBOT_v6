1f5eb8618bb937c21219f192357756c7f4ad5a11
Peter Kajan <sknitro@gmail.com>
2020-07-19 08:52:29 +0200

    -

--- app/function_calculators.js
+++ app/function_calculators.js
@@ -13,3 +13,26 @@ module.exports = {
 
 
 };
+
+
+/*
+
+
+const date = require('date-and-time');
+
+var date1_str = {"year":2020,"month":7,"day":25};
+
+var date1 = date.parse(`${date1_str.year}-${date1_str.month}-${date1_str.day}`, 'YYYY-M-DD');
+
+
+const now = new Date();
+
+var plusSeven = date.addDays(date1,7);
+var diffD = date.subtract(date1,now).toDays();
+var diffH = date.subtract(date1,now).toHours()
+
+console.log(plusSeven);
+
+console.log(diffD,diffH);
+
+*/
