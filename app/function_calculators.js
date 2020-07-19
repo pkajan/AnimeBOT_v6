3d8c81988d32d2b200e58e50479c102d0c5d5d0a
Peter Kajan <sknitro@gmail.com>
2020-07-19 08:19:10 +0200

    ...

---
+++ app/function_calculators.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra');
+const util = require('util');
+const request = require('node-fetch');
+const date = require('date-and-time');
+
+
+module.exports = {
+    name: 'fwASYNC',
+    description: 'ASYNC write into file',
+    execute(filepath, data) {
+        //function
+    },
+
+
+};
