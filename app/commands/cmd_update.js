7226d722a3bac184c1581ff7af50a604ea28c630
Peter Kajan <sknitro@gmail.com>
2020-07-23 07:20:30 +0200

    upd cmd
    timeout throws error when invoked from node.... strange....

--- app/commands/cmd_update.js
+++ app/commands/cmd_update.js
@@ -6,13 +6,14 @@ module.exports = {
 	name: 'update',
 	description: 'update bot from internet/github repo!',
 	execute(data) {
+		log.info(i18n.__("updateStarted", data.message.author.username.toString()));
 		var execSync = require('child_process').execSync;
 		var cmd = `${updateCMD}`;
 
 		var options = {
 			encoding: 'utf8'
 		};
-		log.info(i18n.__("updateStarted", data.message.author.username.toString()));
 		console.log(execSync(`${cmd}`, options));
+		log.info(i18n.__("updateEnded", data.message.author.username.toString()));
 	}
 };
