1d674ef95916d51e0401761d1988a27c07268931
Peter Kajan <sknitro@gmail.com>
2020-07-23 11:53:36 +0200

    upd again

--- app/commands/cmd_update.js
+++ app/commands/cmd_update.js
@@ -7,26 +7,18 @@ module.exports = {
 	description: 'update bot from internet/github repo!',
 	execute(data) {
 		log.info(i18n.__("updateStarted", data.message.author.username.toString()));
-		/*var execSync = require('child_process').execSync;
-		var cmd = `${updateCMD}`;
 
-		var options = {
-			encoding: 'utf8'
-		};
-		console.log(execSync(`${cmd}`, options));
-		log.info(i18n.__("updateEnded", data.message.author.username.toString()));
-		*/
-		
-		
+		var cmdUPD = [
+			"git reset --hard",
+			"git fetch --all",
+			"git pull"
+		];
 
-		const execFile = require('child_process').execFile;
-		const child = execFile('cmd', [`/c ${updateCMD} `], (err, stdout, stderr) => {
-			if (err) {
-				throw err;
-			}
-			console.log(stdout);
-			log.info(i18n.__("updateEnded", data.message.author.username.toString()));
+		cmdUPD.forEach(element => {
+			var spawn = require('child_process').spawnSync;
+			var child = spawn('cmd', [`/c ${element}`]);
+			log.info(child.stdout);
 		});
-
+		log.info(i18n.__("updateEnded", data.message.author.username.toString()));
 	}
 };
