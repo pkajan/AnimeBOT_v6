88f4a5ab0cb8181bf3c7a5f43eb8d5a32fc12b34
Peter Kajan <sknitro@gmail.com>
2020-10-03 19:04:26 +0200

    online-list-download

---
+++ app/commands/cmd_onlinelistdownload.js
@@ -0,0 +1,12 @@
+const log = require('../logger.js');
+const basic = require('../functions_basic');
+
+module.exports = {
+	name: 'onlinelistdownload',
+	altnames: i18n.__({ phrase: "__alt_cmd__onlinelistdownload", locale: "custom" }),
+	description: 'Redownload online list (json) if set in config.',
+	execute(data, args) {
+		log.info(i18n.__("cmd_onlinelistdownload_log", data.message.author.username.toString()));
+		basic.download(data.config.onlineList.url, data.baseAppPATH + "/data/anime.json");
+	},
+};
--- app/crons/03_download_list.js
+++ app/crons/03_download_list.js
@@ -1,18 +1,16 @@
 const log = require('../logger.js');
 const basic = require('../functions_basic.js');
 const { onlineList } = require('../../config/config.json');
-const cronSettings= `0 2 * * *`;
+const cronSettings = `0 2 * * *`;
 var path = require('path');
 var scriptName = path.basename(__filename).substring(0, path.basename(__filename).lastIndexOf('.js'));
+const baseAppPATH = process.cwd();
 
 const CronJob = require('cron').CronJob;
 function task() {
-    var tmpPath = __dirname.substring(0, __dirname.lastIndexOf('\\'));
-    var realPath = tmpPath.substring(0, tmpPath.lastIndexOf('\\')) + "//data//anime.json";
-
     const job = new CronJob(cronSettings, function () {
         if (onlineList.download == true) {
-            basic.download(onlineList.url, realPath);
+            basic.download(onlineList.url, baseAppPATH + "/data/anime.json");
             log.info(i18n.__("cron_3_success"));
         }
     });
