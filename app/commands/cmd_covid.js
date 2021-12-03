6cfe11631234e0e628c57f639d4e6cabc7ec7bd9
Peter Kajan <sknitro@gmail.com>
2021-12-03 16:24:51 +0100

    covid stats
    stats for covid cases from https://mapa.covid.chat/export/csv

---
+++ app/commands/cmd_covid.js
@@ -0,0 +1,31 @@
+/*global i18n*/
+const log = require('../logger.js');
+const discord = require('../functions_discord');
+const basic = require('../functions_basic.js');
+const path = require('path');
+
+module.exports = {
+	name: 'covid',
+	altnames: i18n.__({ phrase: "__alt_cmd__covid", locale: "custom" }),
+	description: 'post covid info',
+	execute(data) {
+		var csvurl = "https://mapa.covid.chat/export/csv";
+		var csvfile = path.normalize(path.join(data.baseAppPATH, "data", "data.csv"));
+		basic.download(csvurl, csvfile);
+
+		setTimeout(function () {
+			var datacsv = basic.readSYNC(csvfile);
+			datacsv = datacsv.split('\n');
+			var tmp = [];
+			datacsv.forEach(val => tmp.push(val.split(';')));
+
+			var COVIDmessage = "**COVID STATS**\n" +
+				"**DATE**: " + tmp[tmp.length - 3][0] + " => " + tmp[tmp.length - 2][0] + "\n" +
+				"**POZIT+**: **" + (tmp[tmp.length - 2][3] - tmp[tmp.length - 3][3]) + "** `[" + tmp[tmp.length - 3][3] + " => " + tmp[tmp.length - 2][3] + "]`\n" +
+				"**DEATH+**: **" + (tmp[tmp.length - 2][4] - tmp[tmp.length - 3][4]) + "** `[" + tmp[tmp.length - 3][4] + " => " + tmp[tmp.length - 2][4] + "]`";
+			discord.replyMSG(data.message, COVIDmessage);
+			log.info(i18n.__("cmd_covid_log", data.message.author.username.toString()));
+		}, 5000);
+
+	},
+};
